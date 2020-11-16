03a7027e1ade935437cee14b7a44b2c37935aa18
Your Name <you@example.com>
2020-11-15 21:59:28 -0600

    first commit

---
+++ src/main/java/com/example/demo/NubeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NubeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/NubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NubeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
